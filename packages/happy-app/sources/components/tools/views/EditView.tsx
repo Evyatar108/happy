8dd494b4ca9a0c499331f6b97aa1c1490cec9082
Evyatar Mitrani <evmitran@microsoft.com>
2026-04-27 18:56:28 -0700

    feat: US-007 - Wire EditView to use CollapsibleDiffPreview

--- packages/happy-app/sources/components/tools/views/EditView.tsx
+++ packages/happy-app/sources/components/tools/views/EditView.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { ToolSectionView } from '../../tools/ToolSectionView';
 import { ToolViewProps } from './_all';
+import { CollapsibleDiffPreview } from '@/components/diff/CollapsibleDiffPreview';
 import { ToolDiffView } from '@/components/tools/ToolDiffView';
 import { knownTools } from '../../tools/knownTools';
 import { trimIdent } from '@/utils/trimIdent';
@@ -21,11 +22,20 @@ export const EditView = React.memo<ToolViewProps>(({ tool }) => {
     return (
         <>
             <ToolSectionView fullWidth>
-                <ToolDiffView 
-                    oldText={oldString} 
-                    newText={newString} 
-                    showLineNumbers={showLineNumbersInToolViews}
-                    showPlusMinusSymbols={showLineNumbersInToolViews}
+                <CollapsibleDiffPreview
+                    oldText={oldString}
+                    newText={newString}
+                    collapsedLines={10}
+                    renderDiff={({ hunks, maxVisibleLines }) => (
+                        <ToolDiffView
+                            oldText={oldString}
+                            newText={newString}
+                            hunks={hunks}
+                            maxVisibleLines={maxVisibleLines}
+                            showLineNumbers={showLineNumbersInToolViews}
+                            showPlusMinusSymbols={showLineNumbersInToolViews}
+                        />
+                    )}
                 />
             </ToolSectionView>
         </>
