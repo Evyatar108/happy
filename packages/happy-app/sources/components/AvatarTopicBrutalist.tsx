72e2229bb5e434be1a0ff6e9ad45ddb06aafe24f
Evyatar Mitrani <evmitran@microsoft.com>
2026-05-10 01:04:50 -0700

    feat: [US-002] - AvatarTopicBrutalist component
    Constraint: Vitest node runner uses mocked RN and expo-image surfaces

---
+++ packages/happy-app/sources/components/AvatarTopicBrutalist.tsx
@@ -0,0 +1,82 @@
+import * as React from "react";
+import { View } from "react-native";
+import { Image } from "expo-image";
+import { allImages, colorPairs } from "./avatarBrutalistAssets";
+import { resolveTopicBrutalistAvatar } from "@/utils/avatarTopic";
+
+interface AvatarTopicBrutalistProps {
+    id: string;
+    summaryText?: string;
+    metadataName?: string;
+    summaryUpdatedAt?: number;
+    flavor?: string | null;
+    pinnedAvatarImageIndex?: number;
+    pinnedAvatarColorIndex?: number;
+    size?: number;
+    square?: boolean;
+    monochrome?: boolean;
+}
+
+export const AvatarTopicBrutalist = React.memo((props: AvatarTopicBrutalistProps) => {
+    const {
+        id,
+        summaryText,
+        metadataName,
+        summaryUpdatedAt,
+        flavor,
+        pinnedAvatarImageIndex,
+        pinnedAvatarColorIndex,
+        size = 32,
+        square = false,
+        monochrome = false,
+    } = props;
+
+    const { imageIndex, colorIndex } = React.useMemo(() => resolveTopicBrutalistAvatar({
+        id,
+        summaryText,
+        name: metadataName,
+        flavor,
+        pinned: pinnedAvatarImageIndex !== undefined && pinnedAvatarColorIndex !== undefined
+            ? { imageIndex: pinnedAvatarImageIndex, colorIndex: pinnedAvatarColorIndex }
+            : null,
+    }), [
+        id,
+        summaryText,
+        metadataName,
+        summaryUpdatedAt,
+        flavor,
+        pinnedAvatarImageIndex,
+        pinnedAvatarColorIndex,
+    ]);
+
+    const imageSource = allImages[imageIndex];
+    const colorPair = colorPairs[colorIndex];
+    const tintColor = monochrome ? '#999999' : colorPair.tint;
+    const backgroundColor = monochrome ? '#F0F0F0' : colorPair.background;
+
+    const dimension = square ? size : size;
+    const borderRadius = square ? 0 : size / 2;
+
+    return (
+        <View
+            style={{
+                width: dimension,
+                height: dimension,
+                borderRadius,
+                backgroundColor,
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}
+        >
+            <Image
+                source={imageSource}
+                style={{
+                    width: dimension * 0.8,
+                    height: dimension * 0.8,
+                    borderRadius
+                }}
+                tintColor={tintColor}
+            />
+        </View>
+    );
+});
